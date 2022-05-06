51f8e99086996cbe1b4c6431754819bf83cab161
herdeybayor <shereefdeen4real@gmail.com>
2022-05-06 13:15:43 +0100

    Exercise 1.4 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -29,7 +31,7 @@ const Header = (props) => {
 };
 
 //using object destructuring assignment
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts: [part1, part2, part3] }) => {
   return (
     <div>
       <Part part={part1.name} exercise={part1.exercises} />
@@ -39,7 +41,7 @@ const Content = ({ part1, part2, part3 }) => {
   );
 };
 
-const Total = ({ part1, part2, part3 }) => {
+const Total = ({ parts: [part1, part2, part3] }) => {
   return (
     <p>
       Number of exercises {part1.exercises + part2.exercises + part3.exercises}
