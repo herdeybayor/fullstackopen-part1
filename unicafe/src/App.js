9cdebaee70a8cb2ea2fb3adae4c5bf6809ac76a1
herdeybayor <shereefdeen4real@gmail.com>
2022-05-06 16:58:06 +0100

    Exercise 1.7 done

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,12 +1,44 @@
 import { useState } from "react";
 
+const Feedback = () => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+    </div>
+  );
+};
+
+const Statistics = () => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <div>
+        <p>good 6</p>
+        <p>neutral 2</p>
+        <p>bad 1</p>
+        <p>all 9</p>
+        <p>average {5 / 9}</p>
+        <p>positive {(6 / 9) * 100} %</p>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Feedback />
+      <Statistics />
+    </div>
+  );
 };
 
 export default App;
